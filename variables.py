0074e14100cc442e16779f2c0c1e78d69f201bf7
Mike Wasasuke <jorgereluniarojas@gmail.com>
2021-09-24 22:27:27 +0800

    first commit

---
+++ variables.py
@@ -0,0 +1,7 @@
+character_name = 'Draken'
+character_age = '35'
+
+print(f'There once was a man named {character_name}, ')
+print(f'he was {character_age} years old. ')
+print(f'He really liked the name {character_name}, ')
+print(f"but didn't like being {character_age}.")
