1f981d5a047f28fb09054519687d7e218a7808b2
Mike Wasasuke <jorgereluniarojas@gmail.com>
2021-09-25 11:38:47 +0800

    changing variable value

--- variables.py
+++ variables.py
@@ -3,5 +3,13 @@ character_age = '35'
 
 print(f'There once was a man named {character_name}, ')
 print(f'he was {character_age} years old. ')
+
+character_name = 'Mikey'
+
 print(f'He really liked the name {character_name}, ')
 print(f"but didn't like being {character_age}.")
+
+string_variable = 'Mabuhay'
+int_variable = 101
+float_variable = 1.01
+boolean_variable = True
