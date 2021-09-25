a2706a29d56a36d1ec39e5603e13bb1dfb6c80c5
Mike Wasasuke <jorgereluniarojas@gmail.com>
2021-09-25 13:59:56 +0800

    numbers and strings py file added

---
+++ numbers.py
@@ -0,0 +1,50 @@
+from math import *
+
+print(234234)
+print(213.6776)
+print(-3423)
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+
+# use parenthesis for order of operations
+print(3 * (4 + 5))
+
+# get the remainder
+print(20 % 3)
+
+#store in a variable
+my_num = -5
+print(my_num)
+
+# convert number into string
+num_to_str = str(my_num)
+
+# can not print 'num + str'
+
+#functions
+
+print(abs(my_num))
+
+# raised to the power of a number
+print(pow(3, 2))
+
+# max value in parameter
+print(max(4, 6, 7, 2))
+
+# minimum value in the parameter
+print(min(4, 6, 7, 2))
+
+#rounding off
+print(round(3.7))
+
+# get the value to the left side of decimal
+print(floor(3.92))
+
+# round up automatically
+print(ceil(3.2))
+
+# square root
+print(sqrt(25))
+
---
+++ string.py
@@ -0,0 +1,30 @@
+# \n to insert new line
+print('Google Developers\nStudent Club TUP-M')
+
+# insert back slash to include literally the character
+print('Mikey\'s gang')
+
+# store string to variable
+phrase = 'Chikapu'
+
+# concatination
+print(phrase + 'is cool.')
+print(f'{phrase} is cool')
+
+#functions
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+
+# how many characters in a string
+print(len(phrase))
+
+# get specific character
+print(phrase[0])
+
+# kung pang ilan yung specific index parameter
+# pwedeng words, letter, etc
+print(phrase.index('k'))
+
+# replace part of the spring
+print(phrase.replace('Chi', 'FITA'))
